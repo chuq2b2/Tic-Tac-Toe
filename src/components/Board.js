3f056a430ab3d4492be904bb6845b138f67969cb
Quynh Chu <98863661+chuq2b2@users.noreply.github.com>
2023-09-04 19:44:32 -0400

    First Commit: Basic of Tic-Tac-Toe

---
+++ src/components/Board.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Box } from './Box'
+import "./Board.css"
+export const Board = ({ board, onClick }) => {
+  return (
+      <div className='board'>
+          {board.map((value, id) => {
+              return <Box value={value} onClick={() => value=== null && onClick(id)} />
+          })}
+      </div>
+  )
+}
